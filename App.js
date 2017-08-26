3b08ad05b7e8a4870cfd323c6a755a5e1b1afcae
d-kang <david.kang714@gmail.com>
2017-08-26 14:29:15 -0700

    Import Welcome Message Component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Welcome from './components/welcome'
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Welcome />
+        <Text>Hello World!</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
