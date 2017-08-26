60fe85eabb665e26e7c9a435e15b43616287c8d6
d-kang <david.kang714@gmail.com>
2017-08-26 14:46:20 -0700

    Migrate server-config

---
+++ server/server-config.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var handler = require('./route-handler');
+var logger = require('morgan')
+var app = express();
+
+app.use(logger('dev'));
+//middleware
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+app.use(express.static(path.join(__dirname, 'build')));
+
+//routes
+app.post('/api/ingredients', handler.ingredients);
+app.post('/api/pastSearches', handler.pastSearches);
+app.post('/api/image', handler.googleCloudSearch);
+app.get('/callback', handler.callback);
+app.post('/api/findOrCreateUser', handler.findOrCreateUser);
+
+module.exports = app;
