3cbaee5c8137eeee1ad7f254a2922bbf7014a7e0
d-kang <david.kang714@gmail.com>
2017-08-26 14:43:24 -0700

    Add A Button That Fetches Sample Movie Data

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Welcome from './components/welcome'
 export default class App extends React.Component {
+  componentDidMount() {
+
+  }
+  getMovieData() {
+    fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => {
+        var res = response.json()
+        console.log('res', res);
+        alert('res ' + JSON.stringify(response))
+      })
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -10,6 +21,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button title="Movie Data" onPress={this.getMovieData.bind(this)} />
       </View>
     );
   }
--- components/welcome.js
+++ components/welcome.js
@@ -6,6 +6,7 @@ export default class Welcome extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Welcome to the application!</Text>
+
       </View>
     );
   }
