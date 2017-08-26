286e9cdda8ef3a57bb6ce20e7589d6258acc85e7
d-kang <david.kang714@gmail.com>
2017-08-26 14:29:00 -0700

    Add Welcome Message Component

---
+++ components/welcome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to the application!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
