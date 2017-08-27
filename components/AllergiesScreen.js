5e432a0a047cf02a9b3a1a377bce31e35d3d5968
Ankit09osr <ankit09osr@gmail.com>
2017-08-27 16:18:39 -0700

    Display response from server in Example component, Axios is working

--- components/AllergiesScreen.js
+++ components/AllergiesScreen.js
@@ -4,6 +4,7 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default class AllergiesScreen extends React.Component {
   render() {
-    return <Text>This is Allergies Screen!</Text>
+    return (<Text>This is Allergies Screen!</Text>);
+
   }
 }
