d6f56708fec244d0dbf2e3e559198c71008f1604
Nick <nhattwick@gmail.com>
2017-08-27 16:24:33 -0700

    Rebase

--- components/PhotoScreen.js
+++ components/PhotoScreen.js
@@ -4,6 +4,6 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default class PhotoScreen extends React.Component {
   render() {
-    return <Text>This is Photo Screen!</Text>
+    return <Text>This is a change on Photo Screen!</Text>
   }
 }
