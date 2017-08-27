df66a745328560fbcf81dcc523549b01fd2a32c2
d-kang <david.kang714@gmail.com>
2017-08-27 13:59:40 -0700

    Componentize Naviagtion

---
+++ components/PhotoScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+
+
+export default class PhotoScreen extends React.Component {
+  render() {
+    return <Text>This is Photo Screen!</Text>
+  }
+}
---
+++ components/TextScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+
+
+export default class TextScreen extends React.Component {
+  render() {
+    return <Text>This is Text Screen!</Text>
+  }
+}
