150afad3e27a2479d5bac8c0e17b48dfdbca3d71
Ankit09osr <ankit09osr@gmail.com>
2017-08-27 12:09:06 -0700

    Add react-navigation to package.json

--- App.js
+++ App.js
@@ -3,37 +3,37 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import Welcome from './components/welcome'
 const address = require('./address');
 
-export default class App extends React.Component {
-  componentDidMount() {
+// export default class App extends React.Component {
+//   componentDidMount() {
 
-  }
-  getMovieData() {
-    fetch(`${address}:8000/api/test`)
-      .then((response) => {
-        var res = response.json()
-        console.log('res', res);
-        alert(response._bodyText)
-      })
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Welcome />
-        <Text>Hello World!</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Button title="Movie Data" onPress={this.getMovieData.bind(this)} />
-      </View>
-    );
-  }
-}
+//   }
+//   getMovieData() {
+//     fetch(`${address}:8000/api/test`)
+//       .then((response) => {
+//         var res = response.json()
+//         console.log('res', res);
+//         alert(response._bodyText)
+//       })
+//   }
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Welcome />
+//         <Text>Hello World!</Text>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//         <Button title="Movie Data" onPress={this.getMovieData.bind(this)} />
+//       </View>
+//     );
+//   }
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
