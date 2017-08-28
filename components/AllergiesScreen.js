9222a154d543cf0fd6c33d92bd91128dfee033cd
Ankit09osr <ankit09osr@gmail.com>
2017-08-27 21:23:59 -0700

    Add _loadData() that modify text present in state in AllergiesScreen.js

--- components/AllergiesScreen.js
+++ components/AllergiesScreen.js
@@ -4,13 +4,20 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default class AllergiesScreen extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       text : '',
       data : []
     };
   }
+
+  _loadData(text) {
+    this.setState({
+      text : text
+    });
+  }
+
   render() {
     return (<Text>This is Allergies Screen!</Text>);
 
