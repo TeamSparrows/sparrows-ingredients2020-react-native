be206fa172058c35428e343499a08a8f1bc6b27a
d-kang <david.kang714@gmail.com>
2017-08-27 20:02:45 -0700

    Move Example Component To Barcode

--- components/BarcodeScreen.js
+++ components/BarcodeScreen.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
-
+import Example from './Example'
 
 export default class BarcodeScreen extends React.Component {
   render() {
-    return <Text>This is Barcode Screen!</Text>
+    return (
+      <View style={{flex:1}}>
+        <Example />
+      </View>
+
+    )
   }
 }
