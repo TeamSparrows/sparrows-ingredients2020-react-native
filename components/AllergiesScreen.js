89d49f708e2accb29e7772956a02c6fac66b1593
Ankit09osr <ankit09osr@gmail.com>
2017-08-27 21:22:37 -0700

    Add constructor to AllergiesScreen and add state to it

--- components/AllergiesScreen.js
+++ components/AllergiesScreen.js
@@ -3,6 +3,14 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 
 export default class AllergiesScreen extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      text : '',
+      data : []
+    };
+  }
   render() {
     return (<Text>This is Allergies Screen!</Text>);
 
