e24d1f42c64132f722a88dcae6872898c2df757f
d-kang <david.kang714@gmail.com>
2017-08-27 18:48:22 -0700

    Add Services to Profile Component

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -3,6 +3,26 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 
 export default class ProfileScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+    }
+    this.submitEmail = this.submitEmail.bind(this);
+  }
+  submitEmail(e) {
+    e.preventDefault();
+    var email = this._email.value
+    var data = { email }
+    this.setState(data)
+    axios.post('/api/test-email', data)
+    .then((res) => {
+      console.log('res', res);
+    })
+    .catch((err) => {
+      console.log('err', err);
+    })
+  }
   render() {
     return <Text>This is Profile Screen!</Text>
   }
--- components/TextScreen.js
+++ components/TextScreen.js
@@ -1,9 +1,120 @@
-import React from 'react';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import axios from 'react-native-axios';
+const address = require('../address');
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image,
+  Keyboard,
+  TextInput,
+  ScrollView
+} from 'react-native';
 
 
 export default class TextScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
+    this.enterText = this.enterText.bind(this);
+    this.getData= this.getData.bind(this);
+    this.postUser= this.postUser.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+  getData() {
+    axios.get(`${address}:9000/api/test`)
+      .then((response) => {
+        // var res = response.json()
+        // console.log('res', res);
+        console.log('Res Data',response.data);
+        Alert.alert(response.data)
+      })
+      .catch((err) => {
+        console.log('err', err);
+      })
+  }
+  postData() {
+    axios.get(`${address}:9000/api/test`)
+      .then((response) => {
+        // var res = response.json()
+        // console.log('res', res);
+        console.log('Res Data',response.data);
+        Alert.alert(response.data)
+      })
+      .catch((err) => {
+        console.log('err', err);
+      })
+  }
+
+  postUser() {
+    var string = this.state.text
+    let data = {email: string}
+    let fetchData = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    }
+
+    fetch(`${address}:8000/api/test`, fetchData)
+      .then(function(res) {
+        alert(res._bodyInit)
+      })
+    }
+  enterText() {
+
+    return (
+      <Text>{this.state.text}</Text>
+    )
+  }
   render() {
-    return <Text>This is Text Screen!</Text>
+
+    return (
+      <View style={styles.container}>
+
+        <Text style={styles.hi} >Hii</Text>
+        <TextInput
+          ref={(input) => this.input = input}
+          style={styles.textInput}
+          onChangeText={(text) => this.setState({text})}
+          placeholder='enter text here'
+          onSubmitEditing={this.postUser}
+          autoCapitalize="characters"
+          autoCorrect={true}
+          keyboardType="email-address"
+          blurOnSubmit={true}
+          keyboardAppearance="dark"
+        />
+        { this.enterText() }
+        <Button title="Movie Data" onPress={this.getData} />
+        <Button title="Post User Email" onPress={this.postUser} />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: '#fff',
+    marginTop: 50,
+  },
+  boxSmall: {
+    width: 200,
+    height: 200,
+    marginBottom: 10,
+    marginRight: 10,
+    backgroundColor: 'skyblue',
+  },
+  textInput: {height: 40, borderColor: 'gray', borderWidth: 1},
+  hi: {color: 'lightblue', fontSize: 30 },
+});
